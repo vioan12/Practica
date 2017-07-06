b9e5a1911bbad310c0315254ba20f94402cba643
Ioan <vivoicu12@gmail.com>
2017-07-06 20:14:41 +0300

    Update JavaApplication_Practica.java

--- JavaApplication_Practica.java
+++ JavaApplication_Practica.java
@@ -12,13 +12,55 @@ import java.sql.*;
  *
  * @author Ioan
  */
+
+public class t_elevi {
+    
+    private int ID;
+    private String Nume;
+    private String Prenume;
+    private String Adresa;
+    private String Telefon;
+
+    public void setID(int value){
+       this.ID=value;
+    }
+    public void setNume(String value){
+       this.Nume=value;
+    }    
+    public void setPrenume(String value){
+       this.Prenume=value;
+    }    
+    public void setAdresa(String value){
+       this.Adresa=value;
+    }
+    public void setTelefon(String value){
+       this.Telefon=value;
+    }
+    public int getID(){
+       return this.ID;
+    }
+    public String getNume(){
+       return this.Nume;
+    }    
+    public String getPrenume(){
+       return this.Prenume;
+    }    
+    public String getAdresa(){
+       return this.Adresa;
+    }
+    public String setTelefon(){
+       return this.Telefon;
+    }
+
+}
+
 public class JavaApplication_Practica {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try
+    try
     {
       // create our mysql database connection
       String myDriver = "org.gjt.mm.mysql.Driver";
