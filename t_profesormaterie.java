c3b737b9570975ea91cd23ef97829b4d6d9e5afe
Ioan <vivoicu12@gmail.com>
2017-07-06 21:24:01 +0300

    Add files via upload

---
+++ t_profesormaterie.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication_practica;
+
+/**
+ *
+ * @author Ioan
+ */
+public class t_profesormaterie {
+    private int Profesor_ID;
+    private int Materie_ID;
+
+    public void setProfesor_ID(int value){
+       this.Profesor_ID=value;
+    }
+    public void setMaterie_ID(int value){
+       this.Materie_ID=value;
+    }    
+    public int getProfesor_ID(){
+       return this.Profesor_ID;
+    }
+    public int getMaterie_ID(){
+       return this.Materie_ID;
+    } 
+}
