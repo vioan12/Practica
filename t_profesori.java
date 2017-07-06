6e6697e35d692dd449ac8637329fe4c5c01f18fb
Ioan <vivoicu12@gmail.com>
2017-07-06 21:21:12 +0300

    Add files via upload

---
+++ t_profesori.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication_practica;
+
+/**
+ *
+ * @author Ioan
+ */
+public class t_profesori {
+    private int ID;
+    private String Nume;
+    private String Prenume;
+    private String Telefon;
+    private String Studii;
+
+    public void setID(int value){
+       this.ID=value;
+    }
+    public void setNume(String value){
+       this.Nume=value;
+    }    
+    public void setPrenume(String value){
+       this.Prenume=value;
+    }    
+    public void setTelefon(String value){
+       this.Telefon=value;
+    }
+    public void setStudii(String value){
+       this.Studii=value;
+    }
+    public int getID(){
+       return this.ID;
+    }
+    public String getNume(){
+       return this.Nume;
+    }    
+    public String getPrenume(){
+       return this.Prenume;
+    }    
+    public String getTelefon(){
+       return this.Telefon;
+    }
+    public String getStudii(){
+       return this.Studii;
+    }
+}
